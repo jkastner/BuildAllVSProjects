7c709b6e79c34dbc4accad78cc9ff7c4dcb6940e
JohnathanK <johnathan.kastner@gmail.com>
2017-03-24 15:32:39 -0600

    Informative tooltips for build status

---
+++ BuildAllVSProjects/Converters/BuildStatusToDescriptionConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using BuildAllVSProjects.Models;
+
+namespace BuildAllVSProjects.Converters
+{
+    class BuildStatusToDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isBuildStatus = value as BuildSuccessStatus? ?? BuildSuccessStatus.Undefined;
+            if (isBuildStatus != BuildSuccessStatus.Undefined)
+            {
+                switch (isBuildStatus)
+                {
+                    case BuildSuccessStatus.Undefined:
+                        break;
+                    case BuildSuccessStatus.NotAttempted:
+                        return "Not attempted";
+                    case BuildSuccessStatus.IsBuilding:
+                        return "Building...";
+                    case BuildSuccessStatus.FailedOnLatest:
+                        return "Build failed";
+                    case BuildSuccessStatus.SucceededOnLatest:
+                        return "Build succeeded";
+                    case BuildSuccessStatus.FailedOnPrevious:
+                        return "Build previously failed";
+                    case BuildSuccessStatus.SucceededOnPrevious:
+                        return "Build previously succeeded";
+                    case BuildSuccessStatus.Exception:
+                        return "Build exception";
+                    default:
+                        return "";
+                }
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BuildAllVSProjects/Models/BuildSuccessStatus.cs
+++ BuildAllVSProjects/Models/BuildSuccessStatus.cs
@@ -1,15 +1,24 @@
 
+using System.ComponentModel;
+
 namespace BuildAllVSProjects.Models
 {
     public enum BuildSuccessStatus
     {
         Undefined,
+        [Description("Not attempted")]
         NotAttempted,
+        [Description("Building...")]
         IsBuilding,
+        [Description("Build failed")]
         FailedOnLatest,
+        [Description("Build succeeded")]
         SucceededOnLatest,
+        [Description("Build previously failed")]
         FailedOnPrevious,
+        [Description("Build previously succeeded")]
         SucceededOnPrevious,
+        [Description("Build exception")]
         Exception
 
     }
